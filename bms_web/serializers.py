85479f3aa3e25bcb329cbacbe8d284e94480d656
abcdoreme <abcdoreme@example.com>
2026-04-20 20:39:25 +0800

    [修改]-修正在页面新增设备时只创建了record实例没有创建device实例的问题

--- bms_web/serializers.py
+++ bms_web/serializers.py
@@ -5,6 +5,16 @@ class RecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Record
         fields = '__all__'
+    
+    def create(self, validated_data):
+        # 创建新的 Device 实例
+        Device.objects.create(mac=validated_data.get('mac'), 
+                        gponsn=validated_data.get('gponsn'),
+                        status = 0)
+        
+        # 创建 Record
+        record = Record.objects.create(**validated_data)
+        return record
 
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
